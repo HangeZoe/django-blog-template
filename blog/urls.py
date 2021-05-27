9d0bc010b882ed3231a3e32e5e92d43c79ba0aa0
Hange Zoe <maryburyak@gmail.com>
2021-05-27 22:00:28 +0200

    feat: add publish button

--- blog/urls.py
+++ blog/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('post/new/', views.post_new, name='post_new'),
     path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
     path('drafts/', views.post_draft_list, name='post_draft_list'),
+    path('post/<pk>/publish/', views.post_publish, name='post_publish'),
 ]
