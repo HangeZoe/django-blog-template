d55fb290907f8f7af0f9b3f28d86ffb1f86c3db5
Hange Zoe <maryburyak@gmail.com>
2021-05-27 21:02:47 +0200

    feat: save new posts as drafts

--- blog/urls.py
+++ blog/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
     path('post/new/', views.post_new, name='post_new'),
     path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
+    path('drafts/', views.post_draft_list, name='post_draft_list'),
 ]
