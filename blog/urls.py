f8af3d58646a441b92ff78c2624c2784ee34fd0e
Hange Zoe <maryburyak@gmail.com>
2021-05-27 22:10:35 +0200

    feat: add delete post button

--- blog/urls.py
+++ blog/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
     path('drafts/', views.post_draft_list, name='post_draft_list'),
     path('post/<pk>/publish/', views.post_publish, name='post_publish'),
+    path('post/<pk>/remove/', views.post_remove, name='post_remove'),
 ]
